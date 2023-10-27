3f447024caf84abb2ec8a87cdaf942b1b2343b1d
Rizky Maolana Firdaus <rizkymaulanafrds@gmail.com>
2023-10-27 17:41:24 +0700

    feat: add file

---
+++ UTS/readme.md
@@ -0,0 +1,13 @@
+Jawaban UTS TESTING DAN QA PERANGKAT LUNAK
+
+Nama : Rizky Maolana Firdaus
+NIM : 201011401034
+kelas : 07TPLE007
+
+referensi
+https://www.dicoding.com/blog/white-box-testing/
+https://www.geeksforgeeks.org/software-engineering-white-box-testing/\
+https://www.turing.com/kb/unit-testing-in-python
+https://codepolitan.com/blog/mengenal-unit-testing-dengan-python-596da4e55cd01
+https://towardsdatascience.com/ci-cd-by-example-in-python-46f1533cb09d
+https://www.niagahoster.co.id/blog/ci-cd-adalah/
---
+++ unittestRizky.py
@@ -0,0 +1,29 @@
+import unittest
+
+symbols = [('M', 1000), ('CM', 900), ('D', 500), ('CD', 400), ('C', 100), ('XC', 90),
+            ('L', 50), ('XL', 40), ('X', 10), ('IX', 9), ('V', 5), ('IV', 4), ('I', 1)]
+
+def romannumeral(number):
+    outstring = ""
+    while number > 0:
+        for symbol, value in symbols:
+            if number - value >= 0:
+                outstring += symbol
+                number = number - value
+                break
+    return outstring
+    
+
+class Test(unittest.TestCase):
+    def test_9(self):
+        self.assertEqual(romannumeral(9), "IX")
+    def test_29(self):
+        self.assertEqual(romannumeral(29),"XXIX")
+    def test_707(self):
+        self.assertEqual(romannumeral(707), "DCCVII")
+    def test_1800(self):
+        self.assertEqual(romannumeral(1800),"MDCCC")
+
+if __name__ == '__main__':
+    number_in = input("Enter a number: ")
+    romannumeral(int(number_in))
